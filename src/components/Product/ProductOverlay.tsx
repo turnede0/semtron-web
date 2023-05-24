126a6e25d614ed2aab9eea704381370cbb5a2be1
alexlam184 <alexwork0211@gmail.com>
2023-05-25 00:47:49 +0800

    feat : add title in product page

--- src/components/Product/ProductOverlay.tsx
+++ src/components/Product/ProductOverlay.tsx
@@ -2,6 +2,13 @@ import { useTranslations } from "next-intl";
 import React from "react";
 import Image from "next/image";
 import playOne from "@public/image/play_1.jpg";
+import { Anton } from "@next/font/google";
+
+const anton = Anton({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-anton",
+});
 
 export default function ProductOverlay() {
   const t = useTranslations("ProductPage");
@@ -33,6 +40,30 @@ export default function ProductOverlay() {
           <div className="text-white text-8xl">{t("carsim")}</div>
         </div>
       </section>
+      <div
+        className="relative flex justify-center items-center h-screen z-10 max-h-full overflow-hidden -mb-1  "
+        id="top"
+        data-section="section1"
+      >
+        <div
+          className={`flex-col flex w-full justify-center items-start ml-52`}
+        >
+          {/* <div className="bg-themedarkblue text-white p-5 text-2xl text-bold">
+            {t("title")}
+          </div>
+          <div className="text-black text-8xl">{t("carsim")}</div> */}
+          <h6 className={`${anton.className} text-8xl`}>
+            <div className="my-5">
+              <span className="text-themeyellow ">{t("message1")} </span>
+              <span className={`text-themedarkblue`}>{t("message2")} </span>
+            </div>
+            <div className="my-5">
+              <span className="text-themeyellow">{t("message3")} </span>
+              <span className={`text-themedarkblue`}>{t("message4")}</span>
+            </div>
+          </h6>
+        </div>
+      </div>
     </div>
   );
 }
