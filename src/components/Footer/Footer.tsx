7d19ab6c6fd0edd313582c339b0fc1415480201e
teeai00001 <84966366+teeai00001@users.noreply.github.com>
2023-05-25 18:47:43 +0800

    footer logo, json: spotlight, product content

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import LogoIcon from "@public/simAuto_logo.svg";
+import aerosimlogo from "@public/image/aerosimLogo.png";
+import recyclingfundlogo from "@public/image/recycling-fund_logo.png";
 import { Link, useTranslations } from "next-intl";
 
 const Footer = () => {
@@ -9,7 +11,17 @@ const Footer = () => {
   return (
     <div className="w-full md:h-[500px] lg:grid lg:grid-cols-6 gap-8 text-white bg-themedarkblue">
       <div className="pl-5 lg:col-start-1 lg:col-end-4">
-        <Image priority src={LogoIcon} alt="logo" />
+        <div className="m-10 " role="company logo badge">
+          <Image priority src={LogoIcon} alt="logo" />
+        </div>
+        <div className="flex mt-10">
+          <div className="m-10 " role="company logo badge">
+            <Image height={70} src={aerosimlogo} alt="aero" />
+          </div>
+          <div className="m-10 h-[50px]" role="company logo badge">
+            <Image height={70} src={recyclingfundlogo} alt="businessplan" />
+          </div>
+        </div>
         {/* <div className="flex justify-between md:w-[75%] my-6"></div> */}
       </div>
       <div className="row-span-2 lg:col-end-6 lg:col-span-2 md:flex md:justify-between mt-7">
--- src/constants/content/news.ts
+++ src/constants/content/news.ts
@@ -1,51 +1,17 @@
-import omron_robot_color from "@public/image/Introducing_OMRON_HD_1500_color.png";
+import dirt_rally from "@public/image/dirt_rally.png";
+import euro_truck from "@public/image/euro_truck.png";
+
 export const News = [
   {
     category: "Industrial Component",
-    title:
-      "OMRON Releases Simulation Software to Accelerate DX at Manufacturing Sites, Including Control Equipment of Partner Companies",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque,exercitationem praesentium nihil.",
-    image_src: omron_robot_color.src,
+    title: "Dirt Rally 2.0",
+    content: "Realistic mini cooper racing experience!",
+    image_src: dirt_rally.src,
   },
   {
     category: "Industrial Component",
-    title:
-      'Dramatically Improving the Effectiveness of Mobile Robot Installation Launch of "FLOW Core 2.1" Fleet Management Software',
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque,exercitationem praesentium nihil.",
-    image_src: omron_robot_color.src,
-  },
-  {
-    category: "Industrial Component",
-    title:
-      "Research paper from OMRON SINIC X presented in top-tier international conference on machine learning research",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque,exercitationem praesentium nihil.",
-    image_src: omron_robot_color.src,
-  },
-  {
-    category: "Industrial Component",
-    title:
-      "OMRON Releases Simulation Software to Accelerate DX at Manufacturing Sites, Including Control Equipment of Partner Companies",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque,exercitationem praesentium nihil.",
-    image_src: omron_robot_color.src,
-  },
-  {
-    category: "Industrial Component",
-    title:
-      'Dramatically Improving the Effectiveness of Mobile Robot Installation Launch of "FLOW Core 2.1" Fleet Management Software',
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque,exercitationem praesentium nihil.",
-    image_src: omron_robot_color.src,
-  },
-  {
-    category: "Industrial Component",
-    title:
-      "Research paper from OMRON SINIC X presented in top-tier international conference on machine learning research",
-    content:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.Voluptatibus quia, nulla! Maiores et perferendis eaque,exercitationem praesentium nihil.",
-    image_src: omron_robot_color.src,
+    title: "Euro Truck",
+    content: "Realistic truck driving and road safety training!",
+    image_src: euro_truck.src,
   },
 ];
