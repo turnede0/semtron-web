44dc10ef64e26d392d6ecba52621f807509b895b
alexlam184 <alexwork0211@gmail.com>
2023-09-19 10:17:28 -0700

    feat : edit color

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -17,7 +17,7 @@ export default function LocaleSwitcher() {
   }
 
   return (
-    <label className=" relative text-white">
+    <label className=" relative text-black">
       <p className="sr-only">{t("label")}</p>
       <select
         className="inline-flex appearance-none bg-transparent py-3 pl-2 pr-6 "
